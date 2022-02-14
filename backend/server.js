6c10655cb249a5308e9cb8a5079da0f68da61701
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2022-02-13 21:01:54 -0600

    feat: first api

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Hello world');
+const express = require('express');
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/api/goals', (req, res) => {
+    res.json({
+        message: 'Get goals'
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
